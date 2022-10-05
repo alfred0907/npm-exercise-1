259c3c006f17e03ab79c97c36de98243f4aff4ce
John Alfred Velasco <jadvelasco07@gmail.com>
2022-10-05 15:20:08 +0800

    Add QR code for contact

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const qrcode = require('qrcode-terminal');
 
 qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;');
+qrcode.generate('BEGIN:VCARD\nN:John Alfred Velasco\nTEL:09052366856\nEMAIL:johnalfred.velasco@siteminder.com\nEND:VCARD');
